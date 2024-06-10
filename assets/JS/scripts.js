bd4eb8f0715c34332d85a2ad7ef0327678e1467c
AnnaJulia3 <annajuliammiranda@gmail.com>
2024-06-10 14:54:07 -0300

    :sparkles: Progressao do pomodoro

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,10 +1,61 @@
 var toggleTheme = document.getElementById("toggleTheme");
+var controlPlayClock = document.getElementById("controlPlayClock");
+var timeText = document.getElementById("timeText");
+
+const pomodoroTimerInSeconds = 1500;
+const shortBreakTimerInSeconds = 100;
+
+var timeNow = 0;
+var timeEnd = 25;
+var initClock = 0;
+var modeClock = 0;
+var stepClock = 1;
+let progressClock;
+var seconds = 0;
+var minutes = 0;
+var hour = 0;
 
 toggleTheme.addEventListener("click", () => {
     let currentTheme = document.documentElement.getAttribute("data-theme");
     if (currentTheme === "light") {
         document.documentElement.setAttribute("data-theme", "dark");
     } else {
-        document.documentElement.setAttribute("data-theme",  "light");
+        document.documentElement.setAttribute("data-theme", "light");
     }
+});
+
+var aux;
+
+function playClockHour() {
+    timeText.textContent = `${hour}:${minutes}:${seconds}`;
+    timeNow = timeNow + stepClock;
+    hour = Math.trunc(timeNow / 360).toString().padStart(2, '0');
+    minutes = Math.trunc(timeNow / 60).toString().padStart(2, '0');
+    seconds = Math.trunc(timeNow % 60).toString().padStart(2, '0');
+}
+
+function playClock() {
+    timeText.textContent = `${minutes}:${seconds}`;
+    timeNow = timeNow + stepClock;
+    minutes = Math.trunc(timeNow / 60).toString().padStart(2, '0');
+    seconds = Math.trunc(timeNow % 60).toString().padStart(2, '0');
+
+    if(timeNow === timeEnd){
+        clearInterval(progressClock);
+    }
+}
+
+controlPlayClock.addEventListener("change", () => {
+    if (controlPlayClock.checked) {
+        timeNow = initClock;
+        minutes = Math.trunc(timeNow).toString().padStart(2, '0');
+        seconds = '00';
+        hour = '00';
+        progressClock = setInterval(playClock, 1000); // Atualiza a cada segundo
+        playClock();
+    }
+    else {
+        clearInterval(progressClock);
+    }
+    // prox a adicionar: funcao minimizar botoes de configuracao em um
 });
