21e23d5be0117a1480b403168b5b9ca45b07588d
AnnaJulia3 <annajuliammiranda@gmail.com>
2024-06-26 16:04:48 -0300

    :sparkles: feat: Adição de alternância de estilos de pomodoro

--- assets/js/timer_execution.js
+++ assets/js/timer_execution.js
@@ -6,15 +6,60 @@ var progress_clock_space = document.getElementById("progress_clock_space");
 
 const TYPE_POMODORO = 0;
 const TYPE_BREAK = 1;
-const TYPE_STOPWATCH = 2; //prox passo funcao indicar tipo stepTimer = 1
-const TYPE_TIMING_DEVICE = 3; //prox passo funcao indicar tipo stepTimer = -1
-var maxTime = 720;
-var pomodoroTime = 25;
-var breakTime = 5;
+const TYPE_STOPWATCH = 2;
+const TYPE_TIMING_DEVICE = 3;
+const MAX_TIME_IN_SECONDS = 43200;  // 12h = 43200s
+var pomodoroTimeInSeconds = 1500;  // 25m = 1500s
+var breakTimeInSeconds = 300;     // 05m = 300s
 var stepTimer = -1;
 var nowType = 0;
-var timerNow, endTime;
+var timerNowInSeconds, endNowInSeconds;
+var initTimeInSeconds = pomodoroTimeInSeconds;
+var endTimeInSeconds = 0;
+var initBreakInSeconds = breakTimeInSeconds;
+var endBreakInSeconds = 0;
 let timerProgress;
+
+function formatMinutes(){
+    timerNowInMinutes = timerNowInSeconds / 60;
+    var minutes = Math.trunc(timerNowInMinutes).toString().padStart(2, '0');
+    var seconds = Math.trunc(timerNowInSeconds % 60).toString().padStart(2, '0');
+    timerText.textContent = `${minutes}:${seconds}`;
+}
+
+// -- Tipo de timer ---
+var selectTypeTimer = document.getElementById("type_timer");
+
+function changeTypeTimer() {
+    nowType = selectTypeTimer.value;
+
+    if (nowType == TYPE_STOPWATCH) {
+        stepTimer = 1;
+        initTimeInSeconds = 0;
+        endTimeInSeconds = MAX_TIME_IN_SECONDS;
+
+    } else if (nowType == 1) { // pomodoro crescente
+        stepTimer = 1;
+        initTimeInSeconds = 0;
+        initBreakInSeconds = 0;
+        endTimeInSeconds = pomodoroTimeInSeconds;
+        endBreakInSeconds = breakTimeInSeconds;
+
+    }
+    else {
+        stepTimer = -1;
+        initTimeInSeconds = pomodoroTimeInSeconds;
+        initBreakInSeconds = breakTimeInSeconds;
+        endTimeInSeconds = 0;
+        endBreakInSeconds = 0;
+    }
+    if (nowType == TYPE_BREAK) nowType = TYPE_POMODORO; // sessão break ou pomodoro crescente 
+    timerNowInSeconds = initTimeInSeconds;
+    endNowInSeconds = endTimeInSeconds;
+    formatMinutes();
+}
+
+// --- Progressão do timer ---
 
 function stopTimer() {
     stopProgressBar();
@@ -24,62 +69,51 @@ function stopTimer() {
 }
 
 function playTimer() {
-    timerNowInMinutes = timerNow / 60;
-    var minutes = Math.trunc(timerNowInMinutes).toString().padStart(2, '0');
-    var seconds = Math.trunc(timerNow % 60).toString().padStart(2, '0');
-    timerText.textContent = `${minutes}:${seconds}`;
+    formatMinutes();
     if (nowType != TYPE_POMODORO) audioBackground.play();
-    if (timerNowInMinutes == endTime) {
+    if (timerNowInSeconds ==  endNowInSeconds) {
         controlPlayTimer.checked = false;
         stopTimer();
-        if (nowType != TYPE_POMODORO){
+        if (nowType != TYPE_POMODORO) {
             add_count_pomodoro();
             audioSucessPomodoro.play();
-        } 
+        }
         else {
             audioEndBreak.play();
         }
     }
-    timerNow += stepTimer;
+    timerNowInSeconds += stepTimer;
 }
 
 controlPlayTimer.addEventListener("click", () => {
     if (controlPlayTimer.checked) {
-        
         audioStartPomodoro.currentTime = 0;
         audioStartPomodoro.play();
-        timerNow = 0;
         buttonMusic.style.display = "none";
         buttonTopGame.style.display = "none";
         progress_clock_space.setAttribute("theme", "pomodoro");
-
         if (!audioEndBreak.paused) {
             audioEndBreak.pause();
             audioEndBreak.currentTime = 0;
         }
-        if (nowType == TYPE_STOPWATCH) {
-            progressBar();
-            endTime = maxTime;
+
+        if (nowType == TYPE_BREAK) {
+            timerNowInSeconds = initBreakInSeconds;
+            endNowInSeconds = endBreakInSeconds;
+
+            nowType = TYPE_POMODORO;
+            buttonTopGame.style.display = "flex";
+            progress_clock_space.setAttribute("theme", "break");
+            not_show_count_pomodoro();
         }
         else {
-            if (nowType == TYPE_BREAK) {
-                endTime = breakTime * 60;
-                nowType = TYPE_POMODORO;
-                buttonTopGame.style.display = "flex";
-                progress_clock_space.setAttribute("theme", "break");
-                not_show_count_pomodoro();
-            }
-            else {
-                progressBar();
-                endTime = pomodoroTime * 60;
-                if (nowType == TYPE_POMODORO) nowType = TYPE_BREAK;
-            }
-            if (stepTimer == -1) {
-                timerNow = endTime;
-                endTime = 0;
+            timerNowInSeconds = initTimeInSeconds;
+            progressBar();
+            if (nowType == TYPE_POMODORO) {
+                nowType = TYPE_BREAK;
+                endNowInSeconds = endTimeInSeconds;
             }
         }
-
         timerProgress = setInterval(playTimer, 1000);
         playTimer();
     }
